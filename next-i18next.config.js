f7b06946b896788c90afa8b9fe667f92fd494f0f
Apple <apple@MacBook-Air.local>
2025-04-27 15:29:03 +0900

    Fix issues and update configuration files

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,6 +3,8 @@ module.exports = {
     locales: ['en', 'ja', 'fr', 'it', 'ar', 'hi', 'ko', 'zh','zh-TW'],  // 対応する言語を指定
     defaultLocale: 'en',  // 初期表示言語を設定
     localeDetection: true,
+    ns: ['common'], // 使用する名前空間（common.jsonを使う）
+ defaultNS: 'common', // デフォルトの名前空間
   },
   reloadOnPrerender: true,  // ビルド時のキャッシュ再読み込み
 };
--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const { i18n } = require('./next-i18next.config');
 module.exports = {
   i18n: {
-    locales: ['en', 'ja', 'fr','It','Ar','Hi','ko', 'de', 'es', 'zh', 'zh-TW'], // 対応する言語を指定
+    locales: ['en', 'ja', 'fr','it','ar','hi','ko','zh', 'zh-TW'], // 対応する言語を指定
     defaultLocale: 'en', // 初期表示言語を日本語に設定
   },
 };
