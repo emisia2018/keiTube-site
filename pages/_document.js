aa8719cfddc67c1b4197f31eebfda3099d017cb6
Apple <apple@MacBook-Air.local>
2025-04-29 11:24:54 +0900

    Exclude large file and reinstall dependencies

---
+++ pages/_document.js
@@ -0,0 +1,31 @@
+// document.js
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          {/* Google tag (gtag.js) */}
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=G-G9NSHYF2Q1`}></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-G9NSHYF2Q1');
+              `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
